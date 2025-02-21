ba65c29d14aec26dd57d5ccc3b5ad8cffdfeefb7
pass <pass@g5c22.cin.ufpe.br>
2025-02-21 19:43:31 -0300

    feat: second problem almost finished

---
+++ List-1/B - Broken Keyboard (a.k.a. Beiju Text)/main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include "LStack.h"
+
+using namespace std;
+
+string beijuText(string text);
+
+int main()
+{
+    string input, result;
+
+
+    while(cin >> input)
+    {
+        cout << beijuText(input) << endl;
+    }
+
+    return 0;
+}
+
+string beijuText(string text)
+{
+    char character;
+    bool changeStack = false;
+    LStack<char> textNormal;
+    LStack<char> textBeiju;
+    LStack<char> textAll;
+
+    for(int i = 0; i < text.length(); i++)
+    {
+        character = text[i];
+        
+        if(character == '[')
+        {
+            changeStack = true;
+        }
+        else if(character == ']')
+        {
+            changeStack = false;
+        }
+
+        if(!changeStack && character != ']')
+        {
+            textNormal.push(character);
+        }
+        else if(changeStack && character != '[')
+        {
+            textBeiju.push(character);
+        }
+    }
+
+    int lenStack = textNormal.length();
+    for(int i = 0; i < lenStack; i++)
+    {
+        textAll.push(textNormal.topValue());
+        textNormal.pop();
+    }
+
+    lenStack = textBeiju.length();
+    for(int i = 0; i < lenStack; i++)
+    {
+        textAll.push(textBeiju.topValue());
+        textBeiju.pop();
+    }
+
+    lenStack = textAll.length();
+    string finalText = "";
+    
+    int i = 0;
+    while(textAll.length() != 0)
+    {
+        finalText += textAll.topValue();
+        cout << finalText[i] << endl;
+        textAll.pop();
+
+        i++;
+    }
+
+    return finalText;
+}
