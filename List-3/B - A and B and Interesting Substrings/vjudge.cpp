52b360ad9d3a9d72c5a2b322b2c9b1ea3cf2f622
Pedro Stelzer <stelzerpedro2@gmail.com>
2025-04-03 08:59:37 -0300

    feat: problem B almost finished

---
+++ List-3/B - A and B and Interesting Substrings/vjudge.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    unordered_map<char, long long> alphabet;
+    unordered_map<char, vector<vector<long long>>> preSums;
+    vector<long long> index;
+    vector<char> letras;
+    string s;
+    long long num = 0;
+
+    for(long long i = 0; i < 26; i++)
+    {   
+        cin >> num;
+        char letter = 'a' + i;
+        alphabet[letter] += num; 
+    }
+
+    cin >> s;
+
+    long long lenghtString = s.length();
+    long long count = 0;
+    long long sum = 0;
+
+    for(long long i = 0; i < lenghtString; i++)
+    {
+        sum += alphabet[s[i]];
+        vector<long long> temp;
+
+        temp.push_back(i);
+        temp.push_back(sum);
+
+        preSums[s[i]].push_back(temp);
+
+        index.push_back(sum);
+
+        bool find = false;
+
+        for(long long j = 0; j < letras.size() && !find; j++) if(s[i] == letras[j]) find = true;
+
+        if(!find) letras.push_back(s[i]);
+    }
+
+    for(long long i = 0; i < preSums.size(); i++)
+    {
+        vector<vector<long long>> list = preSums[letras[i]];
+        long long listSize = list.size();
+
+        if(listSize > 1)
+        {
+            for(long long j = 0; j < listSize-1; j++)
+            {
+
+                for(long long k = j; k < listSize-1; k++)
+                {
+                    bool equal = false;
+                    long long countEqual = 0;
+
+                    if(alphabet[letras[i]] == 0 && (list[j][1] == index[list[k+1][0]-1]))
+                    {
+                        long long soma = list[j][1];
+
+                        for(bool parou = false; j < listSize-1 && !parou; j++)
+                        {
+                            if(list[j][1] == soma && index[list[k+1][0]-1] == soma)
+                                countEqual++;
+                            else
+                            {
+                                parou = true;
+                                j--;
+                            }
+                        }
+
+                        count += (countEqual*(countEqual+1))/2;
+
+                    }
+                    else if(list[j][1] == index[list[k+1][0]-1])
+                        count++;
+                }
+            }
+        }
+    }
+
+    cout << count;
+
+    return 0;
+};
